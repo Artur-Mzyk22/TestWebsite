5588870435a061e5b6a8b25de0da7544a9ca7274
Artur-Mzyk22 <archiemz22@gmail.com>
2025-04-09 01:32:37 +0200

    Adding web map, price list and visual adjustments

--- server.js
+++ server.js
@@ -5,38 +5,49 @@ const path = require('path');
 const server = http.createServer((req, res) => {
   // Set the base directory for static files
   const staticDir = path.join(__dirname, 'images');
-
-  // If the request is for the root URL, serve the HTML file
+  
+  // Serve the main page (index.html)
   if (req.url === '/') {
     const filePath = path.join(__dirname, 'index.html');
-    
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Internal Server Error');
         return;
       }
-
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(data);
     });
   }
-  // If the request is for an image, serve the image file
+  
+  // Serve the price list page (price-list.html)
+  else if (req.url === '/prices.html') {
+    const filePath = path.join(__dirname, 'prices.html');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  }
+  // Serve image files
   else if (req.url.startsWith('/images/')) {
     const imagePath = path.join(staticDir, req.url.split('/images/')[1]);
-    
     fs.readFile(imagePath, (err, data) => {
       if (err) {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Image not found');
         return;
       }
-
       res.writeHead(200, { 'Content-Type': 'image/jpeg' });  // Assuming it's a JPEG
       res.end(data);
     });
   }
   else {
+    console.log(req.url);
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
